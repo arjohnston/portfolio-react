a63c4d7dd1a8cf2d2da657a26c518a7469d6a210
Andrew Johnston <arjohnston3@gmail.com>
2018-02-20 13:13:12 -0800

    update ttt

--- src/pages/projects/tic-tac-toe/index.js
+++ src/pages/projects/tic-tac-toe/index.js
@@ -5,6 +5,8 @@ import ContentSection from '../../../components/content-section/ContentSection'
 const TIE = 'The game was a draw!'
 const PLAYER = 'You won the game!'
 const COMPUTER = 'The computer won the game!'
+const MAX = 10
+const MIN = -10
 
 export default class Tictactoe extends Component {
   constructor (props) {
@@ -20,7 +22,8 @@ export default class Tictactoe extends Component {
         ['', '', ''],
         ['', '', '']
       ],
-      moveCounter: 9
+      moveCounter: 0,
+      minMaxTurnCount: 0
     }
   }
 
@@ -34,157 +37,339 @@ export default class Tictactoe extends Component {
     })
   }
 
-  AI () {
+  minimax (board, player) {
+    let bestScore = -10
+    let currScore = 0
+    let moves = this.getAvailableMoves(board)
     const marker = this.state.selected === 'X' ? 'O' : 'X'
     const opponent = this.state.selected
-    const MAX = 10
-    const MIN = -10
-    let state = { ...this.state }
-    let turnCount = 9 - this.state.moveCounter
-    let move = getBestMove(state.board)
+    let turnCount = this.state.minMaxTurnCount
 
-    function minimax (board, player) {
-      let bestScore = -10
-      let currScore = 0
-      let moves = getAvailableMoves(state.board)
+    // Base case for finding leaf nodes
+    if (turnCount <= 0 || this.checkWinCondition() || !moves) return this.evaluate(board)
 
-      // Base case for finding leaf nodes
-      if (this.state.moveCount <= 0 || this.checkWinCondition() || !moves) return evaluate(board)
-
-      // Maximize
-      if (player === marker) {
-        bestScore = MIN
-        for (let move in moves) {
-          let newBoard = state.board
-          makeMove(newBoard, marker, moves[move])
-          currScore = minimax(newBoard, opponent)
-          if (currScore > bestScore) {
-            bestScore = currScore
-          }
-        }
-        return bestScore
-      }
-
-      // Minimize
-      if (player === opponent) {
-        bestScore = MAX
-        for (let move in moves) {
-          let newBoard = state.board
-          makeMove(newBoard, opponent, moves[move])
-          currScore = minimax(newBoard, marker)
-          if (currScore < bestScore) {
-            bestScore = currScore
-          }
-        }
-        return bestScore
-      }
-    }
-
-    function makeMove (newBoard, player, point) {
-      var row = parseInt(point[0], 10)
-      var col = parseInt(point[1], 10)
-      newBoard[row][col] = player
-      turnCount++
-    }
-
-    function getAvailableMoves (board) {
-      var moves = []
-      // let board = state.board
-
-      for (var row in board) {
-        for (var col in board[row]) {
-          if (board[row][col] === '') moves.push([row, col])
-        }
-      }
-
-      return moves
-    }
-
-     // Gets the best move for this board configuration
-    function getBestMove (board) {
-      let bestScore = MIN
-      let currScore
-      let bestMove = null
-      let moves = getAvailableMoves()
-      let corners = [[0, 0], [0, 2], [2, 0], [2, 2]]
-
-      // Prunes a few options for the first few states
-      if (turnCount === 0) return [1, 1]
-      else if (turnCount === 1 && board[1][1] === '') return [1, 1]
-      else if (turnCount === 1) return corners[Math.floor(Math.random() * 4)]
-
+    // Maximize
+    if (player === marker) {
+      bestScore = MIN
       for (let move in moves) {
-        let newBoard = state.board
-        makeMove(newBoard, marker, moves[move])
-        currScore = minimax(newBoard, opponent)
-
+        let newBoard = [[...board[0]], [...board[1]], [...board[2]]]
+        this.makeMove(newBoard, marker, moves[move])
+        currScore = this.minimax(newBoard, opponent)
         if (currScore > bestScore) {
           bestScore = currScore
-          bestMove = moves[move]
         }
       }
-
-      return bestMove
+      return bestScore
     }
+
+    // Minimize
+    if (player === opponent) {
+      bestScore = MAX
+      for (let move in moves) {
+        let newBoard = [[...board[0]], [...board[1]], [...board[2]]]
+        this.makeMove(newBoard, opponent, moves[move])
+        currScore = this.minimax(newBoard, marker)
+        if (currScore < bestScore) {
+          bestScore = currScore
+        }
+      }
+      return bestScore
+    }
+  }
+
+  makeMove (newBoard, player, point) {
+    let row = parseInt(point[0], 10)
+    let col = parseInt(point[1], 10)
+    newBoard[row][col] = player
+    // let turnCount = this.state.moveCounter
+    // turnCount++
+
+    this.setState((prevState) => {
+      return { minMaxTurnCount: prevState.turnCount++ }
+    })
+
+    return newBoard
+  }
+
+  getAvailableMoves (board) {
+    let moves = []
+
+    for (let row in board) {
+      for (let col in board[row]) {
+        if (board[row][col] === '') moves.push([row, col])
+      }
+    }
+
+    return moves
+  }
+
+  getBestMove () {
+    let board = [[...this.state.board[0]], [...this.state.board[1]], [...this.state.board[2]]]
+    let bestScore = MIN
+    let currScore
+    let bestMove = null
+    let moves = this.getAvailableMoves(board)
+    let corners = [[0, 0], [0, 2], [2, 0], [2, 2]]
+    let turnCount = this.state.minMaxTurnCount
+    const marker = this.state.selected === 'X' ? 'O' : 'X'
+    const opponent = this.state.selected
+    let newBoard = [[...board[0]], [...board[1]], [...board[2]]]
+
+    console.log('called getBestMove')
+
+    // Prunes a few options for the first few states
+    if (turnCount === 0) return [1, 1]
+    else if (turnCount === 1 && board[1][1] === '') return [1, 1]
+    else if (turnCount === 1) return corners[Math.floor(Math.random() * 4)]
+
+    console.log('proceeding to for loop, getBestMove')
+    for (let move in moves) {
+      // let newBoard = [[...board[0]], [...board[1]], [...board[2]]]
+      newBoard = this.makeMove(newBoard, marker, moves[move])
+      currScore = this.minimax(newBoard, opponent)
+
+      console.log(newBoard)
+      console.log('currScore: ', currScore)
+      console.log('bestScore: ', bestScore)
+
+      if (currScore > bestScore) {
+        bestScore = currScore
+        bestMove = moves[move]
+      }
+    }
+    console.log('bestMove: ', bestMove)
+    return bestMove
+  }
+
+  evaluate (board) {
+    let score = 0
+
+    score += this.evaluateLine(board, 0, 0, 0, 1, 0, 2)  // row 0
+    score += this.evaluateLine(board, 1, 0, 1, 1, 1, 2)  // row 1
+    score += this.evaluateLine(board, 2, 0, 2, 1, 2, 2)  // row 2
+    score += this.evaluateLine(board, 0, 0, 1, 0, 2, 0)  // col 0
+    score += this.evaluateLine(board, 0, 1, 1, 1, 2, 1)  // col 1
+    score += this.evaluateLine(board, 0, 2, 1, 2, 2, 2)  // col 2
+    score += this.evaluateLine(board, 0, 0, 1, 1, 2, 2)  // diagonal
+    score += this.evaluateLine(board, 0, 2, 1, 1, 2, 0)  // alternate diagonal
+
+    return score
+  }
+
+  evaluateLine (board, r1, c1, r2, c2, r3, c3) {
+    let score = 0
+    const marker = this.state.selected === 'X' ? 'O' : 'X'
+    const opponent = this.state.selected
+
+    // First cell
+    if (board.gamestate[r1][c1] === marker) score = 1
+    else if (board.gamestate[r1][c1] === opponent) score = -1
+
+    // Second cell
+    if (board.gamestate[r2][c2] === marker) {
+      if (score === 1) score = 10
+      else if (score === -1) return 0
+      else score = 1
+    } else if (board.gamestate[r2][c2] === opponent) {
+      if (score === -1) score = -10
+      else if (score === 1) return 0
+      else score = -1
+    }
+
+    // Final cell
+    if (board.gamestate[r3][c3] === marker) {
+      if (score > 1) score *= 10
+      else if (score < 0) return 0
+      else score = 1
+    } else if (board.gamestate[r3][c3] === opponent) {
+      if (score < 0) score *= 10
+      else if (score > 1) return 0
+      else score = -1
+    }
+    return score
+  }
+
+  AI () {
+    // const state = { ...this.state }
+    const marker = this.state.selected === 'X' ? 'O' : 'X'
+    // const opponent = state.selected
+    // const MAX = 10
+    // const MIN = -10
+    // let turnCount = state.moveCounter
+    let board = [[...this.state.board[0]], [...this.state.board[1]], [...this.state.board[2]]]
+    let move
+    this.setState({
+      minMaxTurnCount: this.state.moveCounter
+    }, () => {
+      move = this.getBestMove()
+
+      try {
+        // console.log('move[0]: ', parseInt(move[0]))
+        // console.log('move[1]: ', parseInt(move[0]))
+        console.log('move: ', move)
+        board[parseInt(move[0], 10)][parseInt(move[1], 10)] = marker
+        // console.log(parseInt(move, 10))
+        // console.log(move[0])
+        // console.log(move[1])
+      } catch (error) {
+        console.log(error)
+      }
+
+      this.setState({
+        board: board,
+        playerTurn: true
+      }, () => this.checkWinCondition())
+    })
+    // let move = this.getBestMove()
+
+    // function minimax (board, player) {
+    //   let bestScore = -10
+    //   let currScore = 0
+    //   let moves = getAvailableMoves(board)
+    //
+    //   // Base case for finding leaf nodes
+    //   if (turnCount <= 0 || this.checkWinCondition() || !moves) return evaluate(board)
+    //
+    //   // Maximize
+    //   if (player === marker) {
+    //     bestScore = MIN
+    //     for (let move in moves) {
+    //       let newBoard = [[...board[0]], [...board[1]], [...board[2]]]
+    //       makeMove(newBoard, marker, moves[move])
+    //       currScore = minimax(newBoard, opponent)
+    //       if (currScore > bestScore) {
+    //         bestScore = currScore
+    //       }
+    //     }
+    //     return bestScore
+    //   }
+    //
+    //   // Minimize
+    //   if (player === opponent) {
+    //     bestScore = MAX
+    //     for (let move in moves) {
+    //       let newBoard = [[...board[0]], [...board[1]], [...board[2]]]
+    //       makeMove(newBoard, opponent, moves[move])
+    //       currScore = minimax(newBoard, marker)
+    //       if (currScore < bestScore) {
+    //         bestScore = currScore
+    //       }
+    //     }
+    //     return bestScore
+    //   }
+    // }
+
+    // function makeMove (newBoard, player, point) {
+    //   var row = parseInt(point[0], 10)
+    //   var col = parseInt(point[1], 10)
+    //   newBoard[row][col] = player
+    //   turnCount++
+    //
+    //   return newBoard
+    // }
+
+    // function getAvailableMoves (board) {
+    //   var moves = []
+    //
+    //   for (var row in board) {
+    //     for (var col in board[row]) {
+    //       if (board[row][col] === '') moves.push([row, col])
+    //     }
+    //   }
+    //
+    //   return moves
+    // }
+
+    // Gets the best move for this board configuration
+    // function getBestMove (board) {
+    //   let bestScore = MIN
+    //   let currScore
+    //   let bestMove = null
+    //   let moves = getAvailableMoves(board)
+    //   let corners = [[0, 0], [0, 2], [2, 0], [2, 2]]
+    //
+    //   // Prunes a few options for the first few states
+    //   if (turnCount === 0) return [1, 1]
+    //   else if (turnCount === 1 && board[1][1] === '') return [1, 1]
+    //   else if (turnCount === 1) return corners[Math.floor(Math.random() * 4)]
+    //
+    //   for (let move in moves) {
+    //     let newBoard = [[...board[0]], [...board[1]], [...board[2]]]
+    //     newBoard = makeMove(newBoard, marker, moves[move])
+    //     currScore = minimax(newBoard, opponent)
+    //
+    //     if (currScore > bestScore) {
+    //       bestScore = currScore
+    //       bestMove = moves[move]
+    //     }
+    //   }
+    //   console.log('bestMove: ', bestMove)
+    //   return bestMove
+    // }
 
     // Evaluates the score for the board passed by checking each line
-    function evaluate (board) {
-      let score = 0
-
-      score += evaluateLine(board, 0, 0, 0, 1, 0, 2)  // row 0
-      score += evaluateLine(board, 1, 0, 1, 1, 1, 2)  // row 1
-      score += evaluateLine(board, 2, 0, 2, 1, 2, 2)  // row 2
-      score += evaluateLine(board, 0, 0, 1, 0, 2, 0)  // col 0
-      score += evaluateLine(board, 0, 1, 1, 1, 2, 1)  // col 1
-      score += evaluateLine(board, 0, 2, 1, 2, 2, 2)  // col 2
-      score += evaluateLine(board, 0, 0, 1, 1, 2, 2)  // diagonal
-      score += evaluateLine(board, 0, 2, 1, 1, 2, 0)  // alternate diagonal
-
-      return score
-    }
+    // function evaluate (board) {
+    //   let score = 0
+    //
+    //   score += evaluateLine(board, 0, 0, 0, 1, 0, 2)  // row 0
+    //   score += evaluateLine(board, 1, 0, 1, 1, 1, 2)  // row 1
+    //   score += evaluateLine(board, 2, 0, 2, 1, 2, 2)  // row 2
+    //   score += evaluateLine(board, 0, 0, 1, 0, 2, 0)  // col 0
+    //   score += evaluateLine(board, 0, 1, 1, 1, 2, 1)  // col 1
+    //   score += evaluateLine(board, 0, 2, 1, 2, 2, 2)  // col 2
+    //   score += evaluateLine(board, 0, 0, 1, 1, 2, 2)  // diagonal
+    //   score += evaluateLine(board, 0, 2, 1, 1, 2, 0)  // alternate diagonal
+    //
+    //   return score
+    // }
 
     // Scores the line by checking each cell for our marker, 1 point for 1, 10 point for 2, 100 for 3, opposite for opponent marker
-    function evaluateLine (board, r1, c1, r2, c2, r3, c3) {
-      let score = 0
+    // function evaluateLine (board, r1, c1, r2, c2, r3, c3) {
+    //   let score = 0
+    //
+    //   // First cell
+    //   if (board.gamestate[r1][c1] === marker) score = 1
+    //   else if (board.gamestate[r1][c1] === opponent) score = -1
+    //
+    //   // Second cell
+    //   if (board.gamestate[r2][c2] === marker) {
+    //     if (score === 1) score = 10
+    //     else if (score === -1) return 0
+    //     else score = 1
+    //   } else if (board.gamestate[r2][c2] === opponent) {
+    //     if (score === -1) score = -10
+    //     else if (score === 1) return 0
+    //     else score = -1
+    //   }
+    //
+    //   // Final cell
+    //   if (board.gamestate[r3][c3] === marker) {
+    //     if (score > 1) score *= 10
+    //     else if (score < 0) return 0
+    //     else score = 1
+    //   } else if (board.gamestate[r3][c3] === opponent) {
+    //     if (score < 0) score *= 10
+    //     else if (score > 1) return 0
+    //     else score = -1
+    //   }
+    //   return score
+    // }
 
-      // First cell
-      if (board.gamestate[r1][c1] === marker) score = 1
-      else if (board.gamestate[r1][c1] === opponent) score = -1
+    // try {
+    //   // console.log('move[0]: ', parseInt(move[0]))
+    //   // console.log('move[1]: ', parseInt(move[0]))
+    //   board[parseInt(move[0], 10)][parseInt(move[1], 10)] = marker
+    //   // console.log(parseInt(move, 10))
+    //   // console.log(move[0])
+    //   // console.log(move[1])
+    // } catch (error) {
+    //   console.log(error)
+    // }
 
-      // Second cell
-      if (board.gamestate[r2][c2] === marker) {
-        if (score === 1) score = 10
-        else if (score === -1) return 0
-        else score = 1
-      } else if (board.gamestate[r2][c2] === opponent) {
-        if (score === -1) score = -10
-        else if (score === 1) return 0
-        else score = -1
-      }
-
-      // Final cell
-      if (board.gamestate[r3][c3] === marker) {
-        if (score > 1) score *= 10
-        else if (score < 0) return 0
-        else score = 1
-      } else if (board.gamestate[r3][c3] === opponent) {
-        if (score < 0) score *= 10
-        else if (score > 1) return 0
-        else score = -1
-      }
-      return score
-    }
-
-    try {
-      state.board[move[0]][move[1]] = marker
-    } catch (error) {
-      console.log(error)
-    }
-
-    this.setState({
-      board: state.board,
-      playerTurn: true
-    }, () => this.checkWinCondition())
+    // this.setState({
+    //   board: board,
+    //   playerTurn: true
+    // }, () => this.checkWinCondition())
   }
 
   handleClick (e) {
@@ -197,7 +382,7 @@ export default class Tictactoe extends Component {
       if (!isNaN(row) && !isNaN(col)) {
         if (board[row][col] === '') {
           board[row][col] = this.state.selected
-          moveCounter--
+          moveCounter++
         }
       }
 
@@ -218,7 +403,7 @@ export default class Tictactoe extends Component {
           ['', '', ''],
           ['', '', '']
         ],
-        moveCounter: 9,
+        moveCounter: 0,
         playerTurn: this.state.selected === 'X'
       }, () => {
         if (!this.state.playerTurn) this.AI()
@@ -271,7 +456,7 @@ export default class Tictactoe extends Component {
       }
       return
     }
-    if (this.state.moveCounter <= 0 && !winner) {
+    if (this.state.moveCounter >= 9 && !winner) {
       this.resetGame({ winner: TIE })
       return
     }
@@ -309,19 +494,37 @@ export default class Tictactoe extends Component {
             <table className='tic-tac-toe-gameboard' onClick={this.handleClick.bind(this)}>
               <tbody>
                 <tr>
-                  <td id='00'>{this.state.board[0][0]}</td>
-                  <td id='01'>{this.state.board[0][1]}</td>
-                  <td id='02'>{this.state.board[0][2]}</td>
+                  <td id='00' className={this.state.board[0][0] !== '' ? 'selected' : ''}>
+                    {this.state.board[0][0]}
+                  </td>
+                  <td id='01' className={this.state.board[0][1] !== '' ? 'selected' : ''}>
+                    {this.state.board[0][1]}
+                  </td>
+                  <td id='02' className={this.state.board[0][2] !== '' ? 'selected' : ''}>
+                    {this.state.board[0][2]}
+                  </td>
                 </tr>
                 <tr>
-                  <td id='10'>{this.state.board[1][0]}</td>
-                  <td id='11'>{this.state.board[1][1]}</td>
-                  <td id='12'>{this.state.board[1][2]}</td>
+                  <td id='10' className={this.state.board[1][0] !== '' ? 'selected' : ''}>
+                    {this.state.board[1][0]}
+                  </td>
+                  <td id='11' className={this.state.board[1][1] !== '' ? 'selected' : ''}>
+                    {this.state.board[1][1]}
+                  </td>
+                  <td id='12' className={this.state.board[1][2] !== '' ? 'selected' : ''}>
+                    {this.state.board[1][2]}
+                  </td>
                 </tr>
                 <tr>
-                  <td id='20'>{this.state.board[2][0]}</td>
-                  <td id='21'>{this.state.board[2][1]}</td>
-                  <td id='22'>{this.state.board[2][2]}</td>
+                  <td id='20' className={this.state.board[2][0] !== '' ? 'selected' : ''}>
+                    {this.state.board[2][0]}
+                  </td>
+                  <td id='21' className={this.state.board[2][1] !== '' ? 'selected' : ''}>
+                    {this.state.board[2][1]}
+                  </td>
+                  <td id='22' className={this.state.board[2][2] !== '' ? 'selected' : ''}>
+                    {this.state.board[2][2]}
+                  </td>
                 </tr>
               </tbody>
             </table>
